83acdd5c2d7751c93a2967d55b90a2727e68575f
Sai Gunturi <sai@nectarom.com>
2016-04-02 14:36:27 -0500

    Add game.

--- Game.elm
+++ Game.elm
@@ -1,39 +1,118 @@
-module Game where
-
+import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
-import Keyboard exposing ( arrows )
+import Color exposing (..)
+import Signal
+import Keyboard
+import Time
+import Debug
+import Window
 
-type Action = Up | Down | Left | Right
+type Direction = Left | Right
+type alias Keys = {x: Int, y: Int}
 
-type alias Player =
-  { x : Int
-  , y : Int
+type alias Model =
+  { x : Float
+  , y : Float
+  , vx : Float
+  , vy : Float
+  , dir : Direction
   }
 
-initPlayer : Player
-initPlayer =
-  { x = 500
-  , y = 500
+robot : Model
+robot =
+  { x = 0
+  , y = 0
+  , vx = 0
+  , vy = 0
+  , dir = Right
   }
 
-limitRange =
-  clamp 0 1000
+walk : Keys -> Model -> Model
+walk keys model =
+  { model |
+      vx = toFloat keys.x * 5,
+      dir =
+        if keys.x < 0 then Left
+        else if keys.x > 0 then Right
+        else model.dir
+  }
+jump : Keys -> Model -> Model
+jump keys model =
+  if keys.y > 0 && model.vy == 0 then
+    { model | vy = 6.0 }
+  else model
 
-movePlayer : {x : Int, y : Int} -> Player -> Player
-movePlayer action player =
-  { player |
-      y = limitRange (player.y - action.y * 10)
-    , x = limitRange (player.x + action.x * 10)
+gravity : Float -> Model -> Model
+gravity dt model =
+  { model |
+      vy = if model.y > 0 then model.vy - dt/4 else 0
   }
 
-render : Player -> Element
-render player =
+
+physics : Float -> Model -> Model
+physics dt model =
+  { model |
+      x = max 0 (model.x + dt * model.vx)
+    , y = max 0 (model.y + dt * model.vy)
+  }
+
+update : (Float, Keys) -> Model -> Model
+update (dt, keys) model =
   let
-    element = show player
+    log = Debug.watchSummary "model" .x model
   in
-    container player.x player.y middle element
+    model
+      |> gravity dt
+      |> walk keys
+      |> jump keys
+      |> physics dt
 
+-- VIEWS
+
+view : (Int, Int) -> Model -> Element
+view (w, h) model =
+  let
+    verb =
+      if model.y > 0 then "jump"
+      else if model.vx /= 0 then "run"
+      else "stand"
+
+    dir =
+      case model.dir of
+        Left -> "left"
+        Right -> "right"
+
+    src = "/assets/game/img/player/" ++ verb ++ "_" ++ dir ++ ".gif"
+
+    player =
+      image 100 100 src
+
+    groundY = 100 - toFloat h/2
+    leftX = 50 - toFloat w/2
+
+    position =
+      (model.x + leftX , model.y + groundY)
+  in
+    collage w h [
+      player
+        |> toForm
+        |> move position
+    ]
+
+-- SIGNALS
+
+input : Signal (Float, Keys)
+input =
+  let
+    delta = Signal.map (\t -> t/20) (Time.fps 30)
+  in
+    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
 
 main : Signal Element
 main =
-  Signal.map render (Signal.foldp movePlayer initPlayer Keyboard.arrows)
+  let
+    state = Signal.foldp update robot input
+  in
+    Signal.map2 view Window.dimensions state
+
+
